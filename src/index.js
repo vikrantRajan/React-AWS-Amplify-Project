0f4c1f549225c9493b9b729de6439edaa8018110
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2022-10-04 00:01:57 -0700

    created PWA deployed with netlify

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import './styles/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register()
