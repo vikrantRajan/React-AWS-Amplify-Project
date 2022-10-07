693ee76ea0afcd4502a39421077d58e913fbebcc
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2022-10-07 02:19:18 -0700

    updated service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
 // import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './styles/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,8 +18,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.register()
+serviceWorkerRegistration.unregister();
 // reportWebVitals();
